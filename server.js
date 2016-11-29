72086dca6bcd09e8bcb802baecca2d6cad167d02
Kate <katelynj29@gmail.com>
2016-11-29 15:32:34 -0500

    finito!

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var path = require('path');
 
+// var whiteboard = require();
+
 var http = require('http');
 var server = http.createServer();
 
@@ -12,18 +14,27 @@ server.on('request', app);
 
 var io = socketio(server);
 
+var picture = [];
+
 io.on('connection', function (socket) {
     /* This function receives the newly connected socket.
        This function will be called for EACH browser that connects to our server. */
     console.log('A new client has connected!');
     console.log(socket.id);
 
+    picture.forEach(function (data) {
+
+      socket.emit('draw', data[0], data[1], data[2]);
+      //whiteboard.draw(data[0], data[1], data[2]);
+    });
+
     socket.on('disconnect', function () {
       console.log(':(');
     });
 
-    socket.on('draw', function (start, end, strokeColor, shouldBroadcast) {
-      socket.broadcast.emit('draw', start, end, strokeColor, shouldBroadcast);
+    socket.on('draw', function (start, end, strokeColor) {
+      picture.push(arguments);
+      socket.broadcast.emit('draw', start, end, strokeColor);
     });
 });
 
